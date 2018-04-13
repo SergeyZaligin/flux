56430afd536471b572887aa591b04c57e770a341
sergey <sergey>
2018-04-13 16:16:12 +0300

    fix

--- src/Counter.js
+++ src/Counter.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import flux from 'flux';
-import { Button } from 'react-bootstrap';
+import { Button, Collapse, Well } from 'react-bootstrap';
 import './Counter.css';
 
 class Counter extends Component {
-
 
   render() {
     return (
       <div className="App">
         <Button bsStyle="info">-</Button>
-        { 0 }
+        &nbsp;{ 0 }&nbsp;
         <Button bsStyle="info">+</Button>
       </div>
     );
